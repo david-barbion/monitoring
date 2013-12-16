780492e2dadd2b4df271512d173eb11c3c32cb82
David Barbion <dbarbion@gmail.com>
2013-12-16 12:35:22 +0100

    improved check_all_diskstat logic

--- check_diskstat/check_all_diskstat.sh
+++ check_diskstat/check_all_diskstat.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-
-CHK=/usr/local/nagios/libexec/check_diskstat.sh
-WARN=${1:-"200,10000,10000"}
-CRIT=${2:-"300,20000,20000"}
+EXITCODE=0
+CHK=/usr/lib/nagios/plugins/check_diskstat.sh
+WARN=${1:-"300,10000,10000"}
+CRIT=${2:-"400,20000,20000"}
 
 for DEVICE in `ls /sys/block`; do
-	if [ -L /sys/block/$DEVICE/device ]; then
-		DEVNAME=$(echo /dev/$DEVICE | sed 's#!#/#g')
-		echo -n "$DEVNAME: "
-		$CHK -d $DEVICE -w $WARN -c $CRIT | sed "s#=#_$DEVNAME=#g"
-	fi
+  if [ -L /sys/block/$DEVICE/device ]; then
+    DEVNAME=$(echo /dev/$DEVICE | sed 's#!#/#g')
+    echo -n "$DEVNAME: "
+    OUTPUT="`$CHK -d $DEVICE -w $WARN -c $CRIT`"
+    STATUS=$?
+    if [ "$EXITCODE" -le "$STATUS" ]; then
+      EXITCODE=$STATUS;
+    fi
+    echo $OUTPUT | sed "s#=#_$DEVNAME=#g"
+  fi
 done
+exit $EXITCODE
