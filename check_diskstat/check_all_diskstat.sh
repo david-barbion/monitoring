e4a749d80fd8e0d8ec3a6171029fbba327e604c7
Sudhamsh Kandukuri <sudhamsh543@gmail.com>
2015-04-16 11:34:39 +0530

    modifying to Show status data for all devices
    modifying to Show status information data for all devices
    case3 in http://nagios.sourceforge.net/docs/3_0/pluginapi.html

--- check_diskstat/check_all_diskstat.sh
+++ check_diskstat/check_all_diskstat.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 EXITCODE=0
 CHK=/usr/lib/nagios/plugins/check_diskstat.sh
-WARN=${1:-"300,10000,10000"}
-CRIT=${2:-"400,20000,20000"}
-
+WARN=${1:-"1200,15000000,15000000"}
+CRIT=${2:-"2000,20000000,20000000"}
+i=0
 for DEVICE in `ls /sys/block`; do
   if [ -L /sys/block/$DEVICE/device ]; then
     DEVNAME=$(echo /dev/$DEVICE | sed 's#!#/#g')
-    echo -n "$DEVNAME: "
-    OUTPUT="`$CHK -d $DEVICE -w $WARN -c $CRIT`"
+    i=`expr $i + 1`
+    OUTPUT="`$CHK -d $DEVICE -w $WARN -c $CRIT -B`"
     STATUS=$?
     if [ "$EXITCODE" -le "$STATUS" ]; then
       EXITCODE=$STATUS;
     fi
-    echo $OUTPUT | sed "s#=#_$DEVNAME=#g"
+    if [ $i -gt 1 ];then
+        perfdatacut=$(echo $OUTPUT | sed "s#=#_$DEVNAME=#g" | cut -d '|' -f2)
+        perfdata=$perfdata'|'$perfdatacut
+        statusdatacut=$(echo $OUTPUT | sed "s#=#_$DEVNAME=#g" | cut -d '|' -f1)
+        statusdata=$statusdata$DEVNAME':'$statusdatacut
+    else 
+        echo $DEVNAME':'$OUTPUT | sed "s#=#_$DEVNAME=#g"
+    fi
   fi
 done
+echo $statusdata
+echo $perfdata
 exit $EXITCODE
+
